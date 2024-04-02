faa1d94bda2db8e9615af4d3afaa8451b873e99f
iMarosKE <maroskutschy@gmail.com>
2024-04-02 22:42:08 +0200

    Cypress wait - added new scenario - wait fro invisible

--- Cypress/e2e/Cypress_Wait.cy.js
+++ Cypress/e2e/Cypress_Wait.cy.js
@@ -15,30 +15,36 @@ describe('Waiting', () => {
     //    cy.get('#confirm_ok_badge', { timeout: 15000 }).should('be.visible' )
     //  })
 
-     it('Waiting for Element to be Visible', () => {
-      cy.visit('../Cypress/testautomation-playground/expected_conditions.html')
-      cy.get('#min_wait').clear().type('10').should('have.value', '10')
-      cy.get('#max_wait').clear().type('10').should('have.value', '10')
+    //  it('Waiting for Element to be Visible', () => {
+    //   cy.visit('../Cypress/testautomation-playground/expected_conditions.html')
+    //   cy.get('#min_wait').clear().type('10').should('have.value', '10')
+    //   cy.get('#max_wait').clear().type('10').should('have.value', '10')
 
-      cy.get('#visibility_target').should('not.be.visible')
-      cy.get('#visibility_trigger').click()
-      cy.get('#visibility_target', { timeout: 15000 }).should('be.visible' )
-      cy.get('div').should('not.contain', 'I just removed my invisibility cloak!!')
-      cy.get('h3').should('not.contain', 'Can you see me?')
-      cy.get('#visibility_target').click()
-      cy.get('div').should('contain', 'I just removed my invisibility cloak!!')
-      cy.get('h3').should('contain', 'Can you see me?')
-      cy.get('div').contains('I just removed my invisibility cloak!!').should('be.visible')
-      cy.get('h3').contains('Can you see me?').should('be.visible')
-    
-//       .expect(Selector('#visibility_target').visible).eql(false)
-//     .click('#visibility_trigger')
-//     const visibilityTargetElement = await Selector('#visibility_target').with({visibilityCheck: true})();
-//     await t.expect(visibilityTargetElement.visible).eql(true)
-//     .expect(Selector('div').withText('I just removed my invisibility cloak!!').visible).eql(false)
-//     .expect(Selector('h3').withText('Can you see me?').visible).eql(false)
-//     .click('#visibility_target')
-//     .expect(Selector('div').withText('I just removed my invisibility cloak!!').visible).eql(true)
-//     .expect(Selector('h3').withText('Can you see me?').visible).eql(true)
-     })
+    //   cy.get('#visibility_target').should('not.be.visible')
+    //   cy.get('#visibility_trigger').click()
+    //   cy.get('#visibility_target', { timeout: 15000 }).should('be.visible' )
+    //   cy.get('div').should('not.contain', 'I just removed my invisibility cloak!!')
+    //   cy.get('h3').should('not.contain', 'Can you see me?')
+    //   cy.get('#visibility_target').click()
+    //   cy.get('div').should('contain', 'I just removed my invisibility cloak!!')
+    //   cy.get('h3').should('contain', 'Can you see me?')
+    //   cy.get('div').contains('I just removed my invisibility cloak!!').should('be.visible')
+    //   cy.get('h3').contains('Can you see me?').should('be.visible')
+    //  })
+
+     it('Waiting for Element to be Invisible', () => {
+        cy.visit('../Cypress/testautomation-playground/expected_conditions.html')
+        cy.get('#min_wait').clear().type('10').should('have.value', '10')
+        cy.get('#max_wait').clear().type('10').should('have.value', '10')
+  
+        cy.get('#invisibility_target').should('be.visible')
+        cy.get('#spinner_gone').should('not.be.visible')
+        cy.get('#invisibility_trigger').click()
+        cy.get('#invisibility_target', { timeout: 15000 }).should('not.be.visible' )
+        cy.get('#spinner_gone').should('be.visible')
+        cy.get('#spinner_gone').contains('Thank God that spinner is gone!').should('be.visible')
+       })
+
+
+
   })
