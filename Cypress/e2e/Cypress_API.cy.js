0d5f597f8f69fee41847cdd773bcdc6dba601a35
iMarosKE <maroskutschy@gmail.com>
2024-04-07 16:41:42 +0200

    updated Cypress API tests

--- Cypress/e2e/Cypress_API.cy.js
+++ Cypress/e2e/Cypress_API.cy.js
@@ -2,13 +2,13 @@ const { request } = require("http")
 
 describe('API Testing', () => {
     
-    it('GET users', () => {
+    it('Search all users', () => {
         cy.request('GET', 'http://localhost:3000/users').then(
             (response) => {
                 expect(response.status).to.eq(200)
-                // expect(response.body).length.to.be.greaterThan(1)
-                // expect(response.body).length.to.be.lessThan(3)
-                // expect(response.body.length).to.eq(2)
+                expect(response.body).length.to.be.greaterThan(1)
+                expect(response.body).length.to.be.lessThan(3)
+                expect(response.body.length).to.eq(2)
                 expect(response.body[0]).to.have.property('id')
                 expect(response.body[0]).to.have.property('name')
                 expect(response.body[0]).to.have.property('age')
@@ -29,11 +29,10 @@ describe('API Testing', () => {
         )
       })
 
-      it('POST user', () => {
+      it('Create user', () => {
         cy.request('POST', 'http://localhost:3000/users', {id: '3', name: 'Susan', age: 50}).then(
             (response) => {
                 expect(response.status).to.eq(201)
-                //expect(response.body.length).to.eq(1)
                 expect(response.body).to.have.property('id')
                 expect(response.body).to.have.property('name')
                 expect(response.body).to.have.property('age')
@@ -45,4 +44,109 @@ describe('API Testing', () => {
             }
         )
       })
+
+      it('Search for created user', () => {
+        cy.request('GET', 'http://localhost:3000/users/3').then(
+            (response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.contain({
+                  id: '3',
+                  name: 'Susan',
+                  age: 50
+                })
+            }
+        )
+      })
+
+      it('Search all users after creation of user', () => {
+        cy.request('GET', 'http://localhost:3000/users').then(
+            (response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body.length).to.eq(3)
+                expect(response.body[0]).to.contain({
+                    id: '1',
+                    name: 'David',
+                    age: 30
+                  });
+                expect(response.body[1]).to.contain({
+                    id: '2',
+                    name: 'John',
+                    age: 40
+                  });
+                  expect(response.body[2]).to.contain({
+                    id: '3',
+                    name: 'Susan',
+                    age: 50
+                  });  
+            }
+        )
+      })
+      
+      it('Update created user', () => {
+        cy.request('PUT', 'http://localhost:3000/users/3', {id: '3', name: 'Susan', age: 60}).then(
+            (response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.contain({
+                  id: '3',
+                  name: 'Susan',
+                  age: 60
+                })
+            }
+        )
+      }) 
+      
+      it('Search for updated user', () => {
+        cy.request('GET', 'http://localhost:3000/users/3').then(
+            (response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.contain({
+                  id: '3',
+                  name: 'Susan',
+                  age: 60
+                })
+            }
+        )
+      })
+
+      it('Delete created user', () => {
+        cy.request('DELETE', 'http://localhost:3000/users/3').then(
+            (response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.contain({
+                  id: '3',
+                  name: 'Susan',
+                  age: 60
+                })
+            }
+        )
+      }) 
+
+      it('Search for deleted user', () => {
+        cy.request({method: 'GET', url: 'http://localhost:3000/users/3', failOnStatusCode: false}).then(
+            (response) => {
+                expect(response.status).to.eq(404)
+                expect(response.body).to.contains('Not Found')
+            }
+        )
+      })
+
+      it('Search all users after delete of created user', () => {
+        cy.request('GET', 'http://localhost:3000/users').then(
+            (response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body.length).to.eq(2)
+                expect(response.body[0]).to.contain({
+                    id: '1',
+                    name: 'David',
+                    age: 30
+                  });
+                expect(response.body[1]).to.contain({
+                    id: '2',
+                    name: 'John',
+                    age: 40
+                  });
+            }
+        )
+      })
+
   })
